41ced2b0b195aee3193e575fc7c5bd0f9f71f818
Jose Manuel Carrichi <josemanuelcarrichi16@gmail.com>
2025-07-24 14:01:55 +0900

    Add supabase variables

---
+++ js/env.js
@@ -0,0 +1,4 @@
+window.env = {
+  SUPABASE_URL: 'https://spudkqhhbovgccrsfnaf.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwdWRrcWhoYm92Z2NjcnNmbmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwOTE3MTgsImV4cCI6MjA2ODY2NzcxOH0.J5RbCi7qXzxAZwNSpWcBfN3NsP8sjW7K-L908aeAcx8',
+}; 
